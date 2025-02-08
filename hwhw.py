c572fdfe9952c396da5b1d50d37bf4e11e79ea5b
AzemaMaksatova <sanzu18haru@gmail.com>
2025-02-09 00:36:29 +0600

    szxdcfvgbh

---
+++ hwhw.py
@@ -0,0 +1,30 @@
+numbers = [1, 2, 2, 3, 3, 4, 5, 6, 7, 7, 8, 9, 9]
+duplicatenumbers = list(dict.fromkeys(numbers))
+print(duplicatenumbers)
+
+
+
+
+words = ["banana", "apple", "cherry", "grape", "watermellon"]
+longest_word = max(words, key=len)
+print(longest_word)
+
+
+
+num = 12345678901234567890
+muskednumber = '*' * (len(str(num))-4) + str(num)[-4:]
+print(muskednumber)
+
+
+
+sentence = "python is a great programming language"
+print(sentence.title())
+
+
+
+
+words = ["banana", "apple", "cherry", "grape"]
+letter = input("letter:")
+for word in words:
+    if letter in word:
+        print(word)
